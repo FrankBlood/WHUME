9f75f2d0a5f694e49737216fc079873328c9a79b
HengDing890 <hengding@whu.edu.cn>
2016-07-07 13:28:06 +0800

    modify Sliding2Blocks.py

--- tools/Sliding2Blocks.py
+++ tools/Sliding2Blocks.py
@@ -3,6 +3,7 @@
 import argparse, os
 import cv2
 from scanner import SliderWindow 
+import numpy as np
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -10,7 +11,7 @@ if __name__ == "__main__":
     parser.add_argument("-WH", help="the height of slider window", type=int)
     parser.add_argument("-SX", help="stride of x", type=int)
     parser.add_argument("-SY", help="stride of y", type=int)
-    parser.add_argument("-I", help="input file path, list all image file path per line", type=str)
+    parser.add_argument("-I", help="input path, list all image file path per line or folder path", type=str)
     parser.add_argument("-O", help="output folder path, the blocks of images will be saved to", type=str)
     args = parser.parse_args()
     
@@ -26,13 +27,27 @@ if __name__ == "__main__":
 
     if not os.path.exists(args.O):
         os.mkdir(args.O)
-
-    fp = open(args.I, 'r')
-    for line in fp.readlines():
-        img = cv2.imread(line.strip())
-        img_name = line[line.rfind("/")+1:].strip()
-        n = 1
-        for x, y, block_img in slider_w.run(img, args.SX, args.SY, args.WW, args.WH):
-            cv2.imwrite(args.O+"/"+str(n)+"-"+img_name, block_img) 
-            n += 1
-    fp.close()
+    
+    if os.path.isfile(args.I):
+        fp = open(args.I, 'r')
+        for line in fp.readlines():
+            img = cv2.imread(line.strip())
+            img_name = line[line.rfind("/")+1:].strip()
+            n = 1
+            for x, y, block_img in slider_w.run(img, args.SX, args.SY, args.WW, args.WH):
+	        w, h, c = np.shape(block_img)
+                if w == h:
+                    cv2.imwrite(args.O+"/"+str(n)+"-"+img_name, block_img) 
+                    n += 1
+        fp.close()
+    elif os.path.isdir(args.I):
+        for img_name in os.listdir(args.I):
+            if img_name.endswith(".jpg"):
+                img_path = args.I+"/"+img_name
+                img = cv2.imread(img_path)
+                n = 1
+                for x, y, block_img in slider_w.run(img, args.SX, args.SY, args.WW, args.WH):
+                    w, h, c = np.shape(block_img)
+                    if w == h:
+                        cv2.imwrite(args.O+"/"+str(n)+"-"+img_name, block_img)
+                        n += 1
